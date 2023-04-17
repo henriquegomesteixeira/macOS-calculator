443045acb461bd10f2e1ac6b3236ebb919232175
Henrique Gomes Teixeira <henriquegomes080012@gmail.com>
2023-04-17 17:21:36 -0300

    novas funcionalidades

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora do macOS
+Este é um projeto de uma calculadora inspirada na calculadora do macOS. Meu primeiro projeto pessoal feito com a intenção de exercita meus aprendizados em HTML, CSS e JavaScript, fiz o mais parecido possível da calculadora MacOs. Sei que poderia ter usado praticas melhores, mas tentei fazer apenas com os aprendizados que tenho até o momento e quero deixa registrado aqui para comporar com minhas melhoras no futuro, e estou muito feliz com o resultado.
+
+# Como utilizar
+Para utilizar a calculadora, basta abrir o arquivo index.html em qualquer navegador web e clicar nos botões numéricos e operadores para realizar as operações desejadas. Também é possível utilizar o teclado para inserir os números e operadores.
+
+# Funcionalidades
+A calculadora realiza as seguintes operações:
+
+Adição (+);
+Subtração (-);
+Multiplicação (*);
+Divisão (/);
+Porcentagem (%);
+Troca de sinal (+/-);
+Todas teclas numericas do teclado funcioinam;
+A tecla "%" realiza o calculo do mesmo;
+A tecla Enter e Barra de espaço do teclado executam o calculo;
+Além disso, a calculadora também possui a funcionalidade de apagar um caractere (backspace) e limpar todo o visor (clear).
+
+![Modelo Feito](image/calculadora.png)
--- script.js
+++ script.js
@@ -4,74 +4,101 @@ const deleteButton = document.querySelector('.delete-button');
 const performCalculation = document.querySelector('.to-execute');
 const signExchange = document.querySelector('.sign-exchange');
 
-let number1 = 0;
-let number2 = 0;
+let operandPrevious = 0;
+let operandCurrent = 0;
 let adicaoClicada = false;
 let subtracaoClicada = false;
 let multiplicacaoClicada = false;
 let divisaoClicada = false;
+let erroDivisão = false;
 
 defaultKey.forEach((element) => {
   element.addEventListener('click', () => {
     if (element.classList.contains('addition')) {
       adicaoClicada = true;
-      number1 = `${result.value}${'+'}`;
-      number2 = result.value;
-      result.value = number2;
+      operandPrevious = `${result.value}${'+'}`;
+      operandCurrent = result.value;
+      result.value = operandCurrent;
     } else if (adicaoClicada) {
       result.value = '';
       adicaoClicada = false;
     }
     if (element.classList.contains('subtraction')) {
       subtracaoClicada = true;
-      number1 = `${result.value}${'-'}`;
-      number2 = result.value;
-      result.value = number2;
+      operandPrevious = `${result.value}${'-'}`;
+      operandCurrent = result.value;
+      result.value = operandCurrent;
     } else if (subtracaoClicada) {
       result.value = '';
       subtracaoClicada = false;
     }
     if (element.classList.contains('multiplication')) {
       multiplicacaoClicada = true;
-      number1 = `${result.value}${'*'}`;
-      number2 = result.value;
-      result.value = number2;
+      operandPrevious = `${result.value}${'*'}`;
+      operandCurrent = result.value;
+      result.value = operandCurrent;
     } else if (multiplicacaoClicada) {
       result.value = '';
       multiplicacaoClicada = false;
     }
     if (element.classList.contains('division')) {
       divisaoClicada = true;
-      number1 = `${result.value}${'/'}`;
-      number2 = result.value;
-      result.value = number2;
+      operandPrevious = `${result.value}${'/'}`;
+      operandCurrent = result.value;
+      result.value = operandCurrent;
+      erroDivisão = true;
     } else if (divisaoClicada) {
       result.value = '';
       divisaoClicada = false;
     }
     if (element.classList.contains('percentage')) {
-      result.value = eval(number1 + (result.value / 100 * number2));
+      result.value = eval(operandPrevious + (result.value / 100 * operandCurrent));
     }
     let valorr = element.value;
     result.value += valorr;
-  })
-})
+  });
+});
 
 deleteButton.addEventListener('click', () => {
   result.value = '';
-  number1 = 0;
-})
+  operandPrevious = 0;
+});
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'Delete' || event.key === 'Backspace') {
     result.value = result.value.slice(0, -1);
+  } else if ((event.key >= '0' && event.key <= '9') || event.key === '.') {
+    if (adicaoClicada || subtracaoClicada || multiplicacaoClicada || divisaoClicada) {
+      result.value = '';
+      adicaoClicada = false;
+      subtracaoClicada = false;
+      multiplicacaoClicada = false;
+      divisaoClicada = false;
+    }
+    result.value += event.key;
+  } else if (event.key === 'Escape') {
+    result.value = '';
+    operandPrevious = 0;
+  } else if (event.key === '-') {
+    result.value = result.value * -1;
+  } else if (event.key === '%') {
+    result.value = eval(operandPrevious + (result.value / 100 * operandCurrent));
+  }
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault();
+    result.value = eval(operandPrevious + result.value);
   }
 });
 
 signExchange.addEventListener('click', () => {
   result.value = result.value * -1;
-})
+});
 
 performCalculation.addEventListener('click', () => {
-  result.value = eval(number1 + result.value);
-})
+  if (erroDivisão === true && result.value === '0') {
+    erroDivisão = false;
+    result.value = "Erro";
+    return;
+  }
+  result.value = eval(operandPrevious + result.value);
+});
